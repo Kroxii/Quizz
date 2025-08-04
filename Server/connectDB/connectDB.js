88a99d46ac8dce0a89af97d27b9827740d3b2b51
Nira00066 <164557554+Nira00066@users.noreply.github.com>
2025-08-04 15:03:02 +0200

    refact

--- Server/connectDB/connectDB.js
+++ Server/connectDB/connectDB.js
@@ -1,5 +1,14 @@
-const sql = require("mysql2");
+const mysql = require("mysql2");
 const dotenv = require("dotenv");
-const { MongoClient } = require("mongodb");
 
 dotenv.config()
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_DATABASE,
+    password: process.env.PASSWORD,
+    waitForConnections:true,
+   
+})
+module.exports = pool.promise();
