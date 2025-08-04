aaab5b36a89838fa18db338c11a76c90e92bf483
Julien <maire94@gmail.com>
2025-08-04 15:53:11 +0200

    merge end

--- Server/connectDB/connectDB.js
+++ Server/connectDB/connectDB.js
@@ -1,3 +1,14 @@
+<<<<<<< HEAD
+const mongoose = require('mongoose')
+
+function connectDB () {    
+    mongoose.connect('mongodb+srv://Julien:quizz@cluster0.mkel2v3.mongodb.net/Quizz')
+        .then(console.log('serveur mongodb connecté'))
+        .catch(error=>console.log(error))
+}
+
+module.exports = connectDB
+=======
 const mysql = require("mysql2");
 const dotenv = require("dotenv");
 
@@ -12,3 +23,4 @@ const pool = mysql.createPool({
    
 })
 module.exports = pool.promise();
+>>>>>>> d6ce2421d391d3381a3820c9e33bafc3eb28a43d
