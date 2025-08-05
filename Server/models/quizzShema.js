b29684996bc743f277f3adad0304521eedb0f8c1
Nira00066 <164557554+Nira00066@users.noreply.github.com>
2025-08-05 18:38:02 +0200

    refacto update router

--- Server/models/quizzShema.js
+++ Server/models/quizzShema.js
@@ -1,5 +1,5 @@
 const { mongoose } = require("mongoose");
-const fs = require('fs');
+
 const quizz = new mongoose.Schema({
   title: { type: String, required: true },
   level: { type: String, required: true },
