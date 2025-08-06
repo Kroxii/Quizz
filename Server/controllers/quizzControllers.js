15419ef2568948419ac61e475e19467efe46cc47
Julien <maire94@gmail.com>
2025-08-06 09:17:43 +0200

    changes

--- Server/controllers/quizzControllers.js
+++ Server/controllers/quizzControllers.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 exports.showQuizz = async function (req, res) {
     try {
         const quizz = await Question.find()
@@ -17,23 +18,12 @@ exports.showQuizz = async function (req, res) {;
    }
 };
 >>>>>>> ba804aed94f64b313ca0fe004bf1cb6a300062f4
-=======
-const Quizz = require("../models/quizzSchema.js");
-exports.showQuizz = async function (req, res) {;
- try {
-     const quizz = await Quizz.find()
-     res.json(quizz)
- } catch (error) {
-     console.log(error)
-   }
-};
->>>>>>> ba804aed94f64b313ca0fe004bf1cb6a300062f4
 
 exports.createQuizz = async function (req, res) {
-    try {
-        let newQuizz = new Quizz(req.body)
-        let savedQuizz = await newQuizz.save()
-    } catch (error) {
-        throw error
-    }
-}
+  try {
+    let newQuizz = new Quizz(req.body);
+    let savedQuizz = await newQuizz.save();
+  } catch (error) {
+    throw error;
+  }
+};
