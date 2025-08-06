21bd97082182ab690ec780ee0961c5456fce9a76
Julien <maire94@gmail.com>
2025-08-06 09:19:05 +0200

    a

--- Server/controllers/quizzControllers.js
+++ Server/controllers/quizzControllers.js
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-exports.showQuizz = async function (req, res) {
-    try {
-        const quizz = await Question.find()
-        res.json(quizz)
-    } catch (error) {
-        console.log(error)
-    }
-}
-=======
 const Quizz = require("../models/quizzSchema.js");
 exports.showQuizz = async function (req, res) {;
  try {
@@ -17,7 +7,6 @@ exports.showQuizz = async function (req, res) {;
      console.log(error)
    }
 };
->>>>>>> ba804aed94f64b313ca0fe004bf1cb6a300062f4
 
 exports.createQuizz = async function (req, res) {
   try {
