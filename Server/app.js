74379e6306f608a2a4084db51073334f5572eae9
Nira00066 <164557554+Nira00066@users.noreply.github.com>
2025-08-07 16:58:27 +0200

    refacto app.js

--- Server/app.js
+++ Server/app.js
@@ -1,19 +1,25 @@
 const express = require("express");
 const app = express();
-const port = 3000;
-const connectDB  = require("./connectDB/connectDB.js");
+const connectDB = require("./connectDB/connectDB.js");
 const question = require("./routers/questionRouter.js");
 const quizz = require("./routers/quizzRouter.js");
+const helmet = require("helmet");
 const cors = require("cors");
 
+const dotenv = require("dotenv");
+dotenv.config();
+const PORT = process.env.PORT;
+
+
 app.use(express.json());
+app.use(helmet());
 app.use(cors());
 
 app.use("/question", question);
 app.use("/quizz", quizz);
 
-app.listen(port, () => {
+app.listen(PORT, () => {
   connectDB();
-  console.log("http://localhost:" + port );
-  console.log("http://127.0.0.1:5500/Front/" );
+// console.log(`${PORT} Lien Server`);
+// console.log(`${BASE_URL}/page Lien Page`);
 });
